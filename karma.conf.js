ae2ad438d2391e0b523e38e1ef41090be684b245
James Goodwin <jamesgoodwin@gmail.com>
2014-02-23 22:00:02 +0000

    Remove unnecessary jasmine lib from karma files list

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,6 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'http://cdnjs.cloudflare.com/ajax/libs/jasmine/2.0.0/jasmine.js',
             'src/*.js',
             'spec/*Spec.js'
         ],
